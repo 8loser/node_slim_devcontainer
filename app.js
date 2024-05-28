1c27f1c96e5b0d0dee5f8019f156f2f8afafd6e7
8loser <8loser@gmail.com>
2024-05-28 17:02:25 +0000

    clean code

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+const indexRouter = require('./routes/index');
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use((_, res) => res.status(404).json({
+    success: false,
+    message: 'page not found',
+}));
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+    try {
+        const data = {
+            title: 'node_slim_devcontainer',
+            description: 'test API',
+        };
+        res.status(200).json({ success: true, data });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
 });
 
 module.exports = router;
