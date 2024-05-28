b3e39767a13aa2fc3445f06e7b85ac94e74ad39e
8loser <8loser@gmail.com>
2024-05-28 16:52:43 +0000

    init eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+    env: {
+        browser: true,
+        node: true,
+    },
+    extends: 'airbnb-base',
+    rules: {
+        // 允許 tab=space*4
+        indent: ['error', 4],
+        // 不檢查 camelcase
+        camelcase: 'off',
+        // 不檢查換行格式 LF, CRLF 都可以
+        'linebreak-style': 0,
+        // 行最大長度
+        'max-len': ['error', { code: 140 }],
+    },
+};
