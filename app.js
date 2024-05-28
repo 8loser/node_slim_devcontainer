2e6d6155961790276112ffb5c1457358f557e62e
8loser <8loser@gmail.com>
2024-05-29 01:25:20 +0800

    add swagger

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const expressJSDocSwagger = require('express-jsdoc-swagger');
+const swaggerOption = require('./config/swaggerConfig');
 
 const app = express();
 
@@ -9,6 +11,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// 載入 swagger
+expressJSDocSwagger(app)(swaggerOption);
+
+/**
+ * 設定 router
+ */
 const indexRouter = require('./routes/index');
 
 app.use('/', indexRouter);
---
+++ config/swaggerConfig.js
@@ -0,0 +1,40 @@
+const swaggerOption = {
+    info: {
+        version: '1.0.0',
+        title: 'node_slim_devcontainer',
+    },
+    servers: [
+        {
+            url: `http://localhost:${process.env.PORT}`,
+            description: 'Local',
+        },
+    ],
+    security: {
+        BearerAuth: {
+            type: 'http',
+            scheme: 'bearer',
+        },
+    },
+    baseDir: __dirname,
+    // Glob pattern to find your jsdoc files (multiple patterns can be added in an array)
+    // filesPattern: './**/*.js',
+    filesPattern: ['../routes/**/*.js'],
+    // URL where SwaggerUI will be rendered
+    swaggerUIPath: '/docs',
+    // Expose OpenAPI UI, 正式站不啟用
+    exposeSwaggerUI: process.env.NODE_ENV === 'development',
+    // Expose Open API JSON Docs documentation in `apiDocsPath` path.
+    exposeApiDocs: false,
+    // Open API JSON Docs endpoint.
+    // apiDocsPath: '/v3/api-docs',
+    // Set non-required fields as nullable by default
+    notRequiredAsNullable: false,
+    // You can customize your UI options.
+    // you can extend swagger-ui-express config. You can checkout an example of this
+    // in the `example/configuration/swaggerOptions.js`
+    swaggerUiOptions: {},
+    // multiple option in case you want more that one instance
+    multiple: true,
+};
+
+module.exports = swaggerOption;
